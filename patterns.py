1be43a751f29ac32f6e5e6e8930bdd8dda407b06
P.Bharathi <bharathi.be115@gmail.com>
2026-04-06 19:56:54 +0530

    Add files via upload

--- patterns.py
+++ patterns.py
@@ -1,24 +1,112 @@
+#PREPARATION
+#1.
 
-n = 10
-for i in range(1, n+1):
-    for j in range(n-i):
-        print(" ", end="")
-    for j in range(i):
-        print("*", end="")
-    print()
+n = 4
 
-    
-n = 5
-for i in range(1, n+1):
-    for j in range(i):
-        print(j, end=" ")
+for i in range(n):          
+    for j in range(n):     
+        print("*", end=" ")
     print()
-    
-n = 5
-for i in range(1, n+1):
-    for j in range(1, 2*n):
-        if j == n-i+1 or j == n+i-1 or i == n:
-            print("*", end="")
+print("------------------------------------------------")    
+#2.
+n = 4
+for i in range(n):
+    for j in range(n):
+        if i == 0 or i == 3 or j == 0 or j ==3:
+            print("*", end=" ")
         else:
-            print(" ", end="")
+            print(" ", end=" ")
     print()
+print("------------------------------------------------")
+
+#3.
+n=6
+for i in range(n): # i=1 nextly i++
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+print("------------------------------------------------")
+#4.
+n=6
+for i in range(n):#n-i =6-1
+    for j in range(n-i):
+        print("*", end=" ")
+    print()
+print("------------------------------------------------")
+
+#5.
+n = 4
+
+for i in range(n):    
+    for j in range(n):
+        if j < n - i - 1:#4-0-1=3 =0<3=true so space goes continue check 0-4-1=3
+                         #3<3 = false comes print else part *
+            print(" ", end=" ")
+        else:
+            print("*", end=" ")
+    print()
+print("------------------------------------------------")
+#6.
+n = 4
+
+for i in range(n):    
+    for j in range(n):
+        if j < i:#0<0
+            print(" ", end=" ")
+        else:
+            print("*", end=" ")
+    print()
+print("------------------------------------------------")
+#7.
+n = 6
+ 
+for i in range(n):    
+    for j in range(i,n):
+        print(" ", end=" ")
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+print("------------------------------------------------")
+#8.
+n = 6
+for i in range(n):
+    for j in range(n):
+        if j ==n-i-1:
+            print(" ", end=" ")
+        else:
+            print("*", end=" ")
+    
+    print()
+
+print("------------------------------------------------")
+#9.
+n = 6
+
+for i in range(n):
+    for j in range(n):
+        if j < n - i - 1:
+            print(" ", end=" ")
+        else:
+            print("*", end=" ")
+    
+    for j in range(i):
+        print("*", end=" ")
+        
+    print()
+
+
+print("------------------------------------------------")
+
+#10
+n = 8
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j ==0 or i == 7 or j == 7:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
+print("------------------------------------------------")
+
+    
